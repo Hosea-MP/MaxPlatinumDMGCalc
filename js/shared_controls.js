e5756b61071f0e897fac3c321aa5659166fc0287
KinglerChamp <130119311+KinglerChamp@users.noreply.github.com>
2024-02-08 23:45:02 -0500

    Searchable trainer names

--- js/shared_controls.js
+++ js/shared_controls.js
@@ -1379,10 +1379,15 @@ function allPokemon(selector) {
 	return allSelector;
 }
 
-function loadCustomList(id) {
-	$("#" + id + " .set-selector").select2({
-		formatResult: function (set) {
-			return (set.nickname ? set.pokemon + " (" + set.nickname + ")" : set.id);
+function loadDefaultLists() {
+	$(".set-selector").select2({
+		formatResult: function (object) {
+			if ($("#randoms").prop("checked")) {
+				return object.pokemon;
+			} else {
+				// return object.text;
+				return object.set ? ("&nbsp;&nbsp;&nbsp;" + object.text) : ("<b>" + object.text + "</b>");
+			}
 		},
 		query: function (query) {
 			var pageSize = 30;
@@ -1390,12 +1395,18 @@ function loadCustomList(id) {
 			var options = getSetOptions();
 			for (var i = 0; i < options.length; i++) {
 				var option = options[i];
-				var pokeName = option.pokemon.toUpperCase();
-				var setName = option.set ? option.set.toUpperCase() : option.set;
-				if (option.isCustom && option.set && (!query.term || query.term.toUpperCase().split(" ").every(function (term) {
-					return pokeName.indexOf(term) === 0 || pokeName.indexOf("-" + term) >= 0 || pokeName.indexOf(" " + term) >= 0 || setName.indexOf(term) === 0 || setName.indexOf("-" + term) >= 0 || setName.indexOf(" " + term) >= 0;
-				}))) {
-					results.push(option);
+				// var pokeName = option.pokemon.toUpperCase();
+				var fullName = option.text.toUpperCase();
+				if (!query.term || query.term.toUpperCase().split(" ").every(function (term) {
+					// return pokeName.indexOf(term) === 0 || pokeName.indexOf("-" + term) >= 0;
+					return fullName.indexOf(term) === 0 || fullName.indexOf("-" + term) >= 0 || fullName.indexOf(" " + term) >= 0 || fullName.indexOf("(" + term) >= 0;
+					// return fullName.indexOf(term) === 0 || fullName.indexOf("-" + term) >= 0 || fullName.indexOf("(" + term) >= 0;
+				})) {
+					if ($("#randoms").prop("checked")) {
+						if (option.id) results.push(option);
+					} else {
+						results.push(option);
+					}
 				}
 			}
 			query.callback({
@@ -1404,8 +1415,7 @@ function loadCustomList(id) {
 			});
 		},
 		initSelection: function (element, callback) {
-			var data = "";
-			callback(data);
+			callback(getFirstValidSetOption());
 		}
 	});
 }
